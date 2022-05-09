43f89dae3798f44ec2a28ec2f42b5f59978ed262
lumoba03 <lumoba03@floridauniversitaria.es>
2022-05-09 19:18:49 +0200

    Diseño de la pantalla de login.

--- Programacion/Login.cs
+++ Programacion/Login.cs
@@ -10,11 +10,12 @@ using System.Windows.Forms;
 
 namespace Programacion
 {
-    public partial class Form1 : Form
+    public partial class Login : Form
     {
-        public Form1()
+        public Login()
         {
             InitializeComponent();
         }
+
     }
 }
