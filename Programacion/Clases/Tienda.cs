b3e57a958c9be6902bf850b8ce299ea41c887b57
DESKTOP-774G3BU\Jaime <jacaar@floridauniversitaria.es>
2022-05-11 17:21:57 +0200

    Agregacion Metodos AgregarProducto, YaEsta y EliminarProducto a la clase Tienda

--- Programacion/Clases/Tienda.cs
+++ Programacion/Clases/Tienda.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
 
 namespace ProyectoIntegradoVerde.Clases
 {
@@ -36,8 +38,48 @@ namespace ProyectoIntegradoVerde.Clases
         //Metodos
 
 
+        static public int AgregarProducto(MySqlConnection conexion, Tienda prod)
+        {
+            int retorno;
 
+            string consulta = String.Format("INSERT INTO tienda (id_prod,nombre,coste,descripcion) VALUES " +
+                "('{0}','{1}','{2}','{3}')", prod.id_prod, prod.nombre, prod.coste, prod.descripcion);
+
+            MySqlCommand comando = new MySqlCommand(consulta, conexion);
+            retorno = comando.ExecuteNonQuery();
+
+            return retorno;
+        }
+
+        public bool YaEsta(MySqlConnection conexion, string nom)
+        {
+            string consulta = string.Format("SELECT * FROM tienda" +
+            " WHERE nombre='{0}'", nom);
+
+            MySqlCommand comando = new MySqlCommand(consulta, conexion);
+            MySqlDataReader reader = comando.ExecuteReader();
+            if (reader.HasRows)
+            { 
+                reader.Close();  
+                return true;
+            }
+            else
+            {
+                reader.Close(); 
+                return false;
+            }
+        }
+
+        public static int EliminarProducto(MySqlConnection conexion, string nom)
+        {
+            int retorno;
+
+            string consulta = string.Format("DELETE FROM tienda WHERE nom={0}", nom);
+            MySqlCommand comando = new MySqlCommand(consulta, conexion);
+            retorno = comando.ExecuteNonQuery();
+            return retorno;
+        }
 
 
     }
-}
+    }
