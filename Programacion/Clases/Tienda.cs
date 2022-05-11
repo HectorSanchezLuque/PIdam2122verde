0d0754f87422f9054b8a156fd82539320faef801
DESKTOP-774G3BU\Jaime <jacaar@floridauniversitaria.es>
2022-05-11 17:09:12 +0200

    Commit base clase Tienda

--- Programacion/Clases/Tienda.cs
+++ Programacion/Clases/Tienda.cs
@@ -8,5 +8,36 @@ namespace ProyectoIntegradoVerde.Clases
 {
     internal class Tienda
     {
+        // Atributos
+
+        private int id_prod;
+        private string nombre;
+        private int coste;
+        private string descripcion;
+
+        //Constructor completo
+        public Tienda(int id ,string nom, int cost, string desc)
+        {
+            id_prod = id;
+            nombre = nom;
+            coste = cost;
+            descripcion = desc;
+        }
+
+        //Constructor vacio
+        public Tienda() { }
+
+        //Getters y Setters
+        public int Id_prod { get => id_prod; set => id_prod = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public int Coste { get => coste; set => coste = value; }
+        public string Descripcion { get => descripcion; set => descripcion = value; }
+
+        //Metodos
+
+
+
+
+
     }
 }
