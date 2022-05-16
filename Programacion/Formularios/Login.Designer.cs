e0cbd3d38c72efb7e0647946b04edfa5a642bd5c
Alvaro Pérez Alborch <alpeal01@floridauniversitaria.es>
2022-05-16 16:25:27 +0200

    chupi

--- Programacion/Formularios/Login.Designer.cs
+++ Programacion/Formularios/Login.Designer.cs
@@ -114,6 +114,7 @@
             this.btnRegistro.TabIndex = 8;
             this.btnRegistro.Text = "Registrar usuario";
             this.btnRegistro.UseVisualStyleBackColor = true;
+            this.btnRegistro.Click += new System.EventHandler(this.btnRegistro_Click);
             // 
             // lblLang
             // 
