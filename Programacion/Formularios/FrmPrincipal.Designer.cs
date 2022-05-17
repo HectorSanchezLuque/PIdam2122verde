2525a1873b170ad00247732f4b63f530b3e67132
ALUMNADO\jacaar <jacaar@floridauniversitaria.es>
2022-05-17 16:40:22 +0200

    Boton Salida FrmPrincipal

--- Programacion/Formularios/FrmPrincipal.Designer.cs
+++ Programacion/Formularios/FrmPrincipal.Designer.cs
@@ -30,13 +30,13 @@ namespace ProyectoIntegradoVerde.Formularios
         private void InitializeComponent()
         {
             this.lblAbreFormulario = new System.Windows.Forms.Label();
+            this.btnTienda = new System.Windows.Forms.Button();
+            this.btnCorreo = new System.Windows.Forms.Button();
+            this.btnReuniones = new System.Windows.Forms.Button();
+            this.btnCalendario = new System.Windows.Forms.Button();
+            this.btnTareas = new System.Windows.Forms.Button();
             this.btnCerrarSesion = new System.Windows.Forms.Button();
             this.ptbInfo = new System.Windows.Forms.PictureBox();
-            this.btnTareas = new System.Windows.Forms.Button();
-            this.btnCalendario = new System.Windows.Forms.Button();
-            this.btnReuniones = new System.Windows.Forms.Button();
-            this.btnCorreo = new System.Windows.Forms.Button();
-            this.btnTienda = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.ptbInfo)).BeginInit();
             this.SuspendLayout();
             // 
@@ -53,13 +53,63 @@ namespace ProyectoIntegradoVerde.Formularios
             this.lblAbreFormulario.MouseEnter += new System.EventHandler(this.lblAbreFormulario_MouseEnter);
             this.lblAbreFormulario.MouseLeave += new System.EventHandler(this.lblAbreFormulario_MouseLeave);
             // 
+            // btnTienda
+            // 
+            this.btnTienda.Image = global::ProyectoIntegradoVerde.Properties.Resources._3837963_shop_business_store_market_icon;
+            this.btnTienda.Location = new System.Drawing.Point(196, 454);
+            this.btnTienda.Name = "btnTienda";
+            this.btnTienda.Size = new System.Drawing.Size(213, 172);
+            this.btnTienda.TabIndex = 14;
+            this.btnTienda.UseVisualStyleBackColor = true;
+            this.btnTienda.Click += new System.EventHandler(this.btnTienda_Click);
+            // 
+            // btnCorreo
+            // 
+            this.btnCorreo.Image = global::ProyectoIntegradoVerde.Properties.Resources._7208644_application_mail_email_message_icon;
+            this.btnCorreo.Location = new System.Drawing.Point(337, 268);
+            this.btnCorreo.Name = "btnCorreo";
+            this.btnCorreo.Size = new System.Drawing.Size(213, 172);
+            this.btnCorreo.TabIndex = 13;
+            this.btnCorreo.UseVisualStyleBackColor = true;
+            this.btnCorreo.Click += new System.EventHandler(this.btnCorreo_Click);
+            // 
+            // btnReuniones
+            // 
+            this.btnReuniones.Image = global::ProyectoIntegradoVerde.Properties.Resources._2932353_group_people_team_hr_humans_icon;
+            this.btnReuniones.Location = new System.Drawing.Point(45, 268);
+            this.btnReuniones.Name = "btnReuniones";
+            this.btnReuniones.Size = new System.Drawing.Size(213, 172);
+            this.btnReuniones.TabIndex = 12;
+            this.btnReuniones.UseVisualStyleBackColor = true;
+            this.btnReuniones.Click += new System.EventHandler(this.btnReuniones_Click);
+            // 
+            // btnCalendario
+            // 
+            this.btnCalendario.Image = global::ProyectoIntegradoVerde.Properties.Resources._8725528_calendar_alt_icon;
+            this.btnCalendario.Location = new System.Drawing.Point(337, 81);
+            this.btnCalendario.Name = "btnCalendario";
+            this.btnCalendario.Size = new System.Drawing.Size(213, 172);
+            this.btnCalendario.TabIndex = 11;
+            this.btnCalendario.UseVisualStyleBackColor = true;
+            this.btnCalendario.Click += new System.EventHandler(this.btnCalendario_Click);
+            // 
+            // btnTareas
+            // 
+            this.btnTareas.Image = global::ProyectoIntegradoVerde.Properties.Resources._8396405_clipboard_note_document_report_paper_icon;
+            this.btnTareas.Location = new System.Drawing.Point(45, 81);
+            this.btnTareas.Name = "btnTareas";
+            this.btnTareas.Size = new System.Drawing.Size(213, 172);
+            this.btnTareas.TabIndex = 10;
+            this.btnTareas.UseVisualStyleBackColor = true;
+            this.btnTareas.Click += new System.EventHandler(this.btnTareas_Click);
+            // 
             // btnCerrarSesion
             // 
-            this.btnCerrarSesion.Location = new System.Drawing.Point(470, 648);
+            this.btnCerrarSesion.Image = global::ProyectoIntegradoVerde.Properties.Resources.Exit;
+            this.btnCerrarSesion.Location = new System.Drawing.Point(541, 624);
             this.btnCerrarSesion.Name = "btnCerrarSesion";
-            this.btnCerrarSesion.Size = new System.Drawing.Size(132, 38);
+            this.btnCerrarSesion.Size = new System.Drawing.Size(61, 62);
             this.btnCerrarSesion.TabIndex = 9;
-            this.btnCerrarSesion.Text = "Cerrar Sesión";
             this.btnCerrarSesion.UseVisualStyleBackColor = true;
             this.btnCerrarSesion.Click += new System.EventHandler(this.btnCerrarSesion_Click);
             // 
@@ -72,56 +122,6 @@ namespace ProyectoIntegradoVerde.Formularios
             this.ptbInfo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.ptbInfo.TabIndex = 1;
             this.ptbInfo.TabStop = false;
-            // 
-            // btnTareas
-            // 
-            this.btnTareas.Image = global::ProyectoIntegradoVerde.Properties.Resources._8396405_clipboard_note_document_report_paper_icon;
-            this.btnTareas.Location = new System.Drawing.Point(45, 81);
-            this.btnTareas.Name = "btnTareas";
-            this.btnTareas.Size = new System.Drawing.Size(213, 172);
-            this.btnTareas.TabIndex = 10;
-            this.btnTareas.UseVisualStyleBackColor = true;
-            this.btnTareas.Click += new System.EventHandler(this.btnTareas_Click);
-            // 
-            // btnCalendario
-            // 
-            this.btnCalendario.Image = global::ProyectoIntegradoVerde.Properties.Resources._8725528_calendar_alt_icon;
-            this.btnCalendario.Location = new System.Drawing.Point(337, 81);
-            this.btnCalendario.Name = "btnCalendario";
-            this.btnCalendario.Size = new System.Drawing.Size(213, 172);
-            this.btnCalendario.TabIndex = 11;
-            this.btnCalendario.UseVisualStyleBackColor = true;
-            this.btnCalendario.Click += new System.EventHandler(this.btnCalendario_Click);
-            // 
-            // btnReuniones
-            // 
-            this.btnReuniones.Image = global::ProyectoIntegradoVerde.Properties.Resources._2932353_group_people_team_hr_humans_icon;
-            this.btnReuniones.Location = new System.Drawing.Point(45, 268);
-            this.btnReuniones.Name = "btnReuniones";
-            this.btnReuniones.Size = new System.Drawing.Size(213, 172);
-            this.btnReuniones.TabIndex = 12;
-            this.btnReuniones.UseVisualStyleBackColor = true;
-            this.btnReuniones.Click += new System.EventHandler(this.btnReuniones_Click);
-            // 
-            // btnCorreo
-            // 
-            this.btnCorreo.Image = global::ProyectoIntegradoVerde.Properties.Resources._7208644_application_mail_email_message_icon;
-            this.btnCorreo.Location = new System.Drawing.Point(337, 268);
-            this.btnCorreo.Name = "btnCorreo";
-            this.btnCorreo.Size = new System.Drawing.Size(213, 172);
-            this.btnCorreo.TabIndex = 13;
-            this.btnCorreo.UseVisualStyleBackColor = true;
-            this.btnCorreo.Click += new System.EventHandler(this.btnCorreo_Click);
-            // 
-            // btnTienda
-            // 
-            this.btnTienda.Image = global::ProyectoIntegradoVerde.Properties.Resources._3837963_shop_business_store_market_icon;
-            this.btnTienda.Location = new System.Drawing.Point(196, 454);
-            this.btnTienda.Name = "btnTienda";
-            this.btnTienda.Size = new System.Drawing.Size(213, 172);
-            this.btnTienda.TabIndex = 14;
-            this.btnTienda.UseVisualStyleBackColor = true;
-            this.btnTienda.Click += new System.EventHandler(this.btnTienda_Click);
             // 
             // FrmPrincipal
             // 
--- Programacion/Properties/Resources.Designer.cs
+++ Programacion/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace ProyectoIntegradoVerde.Properties {
     // a través de una herramienta como ResGen o Visual Studio.
     // Para agregar o quitar un miembro, edite el archivo .ResX y, a continuación, vuelva a ejecutar ResGen
     // con la opción /str o recompile su proyecto de VS.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -146,6 +146,16 @@ namespace ProyectoIntegradoVerde.Properties {
         internal static System.Drawing.Bitmap engFlag {
             get {
                 object obj = ResourceManager.GetObject("engFlag", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Exit {
+            get {
+                object obj = ResourceManager.GetObject("Exit", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
