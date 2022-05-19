b39e8f24b1183ac736afb79e5208125ed0a6fde4
Alvaro Pérez Alborch <alpeal01@floridauniversitaria.es>
2022-05-19 11:02:46 +0200

    añadidod modo oscuro en el perfil del usuario

--- Programacion/Formularios/Soporte.Designer.cs
+++ Programacion/Formularios/Soporte.Designer.cs
@@ -48,9 +48,10 @@
             this.tabSoporte.Controls.Add(this.tabPague1);
             this.tabSoporte.Controls.Add(this.tabPage2);
             this.tabSoporte.Location = new System.Drawing.Point(0, 0);
+            this.tabSoporte.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tabSoporte.Name = "tabSoporte";
             this.tabSoporte.SelectedIndex = 0;
-            this.tabSoporte.Size = new System.Drawing.Size(576, 260);
+            this.tabSoporte.Size = new System.Drawing.Size(432, 211);
             this.tabSoporte.TabIndex = 0;
             // 
             // tabPague1
@@ -58,10 +59,11 @@
             this.tabPague1.Controls.Add(this.btnInforme);
             this.tabPague1.Controls.Add(this.txtInforme);
             this.tabPague1.Controls.Add(this.lbltitle);
-            this.tabPague1.Location = new System.Drawing.Point(4, 25);
+            this.tabPague1.Location = new System.Drawing.Point(4, 22);
+            this.tabPague1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tabPague1.Name = "tabPague1";
-            this.tabPague1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPague1.Size = new System.Drawing.Size(568, 231);
+            this.tabPague1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPague1.Size = new System.Drawing.Size(424, 185);
             this.tabPague1.TabIndex = 0;
             this.tabPague1.Text = "Notificar error";
             this.tabPague1.UseVisualStyleBackColor = true;
@@ -69,9 +71,10 @@
             // 
             // btnInforme
             // 
-            this.btnInforme.Location = new System.Drawing.Point(219, 188);
+            this.btnInforme.Location = new System.Drawing.Point(164, 153);
+            this.btnInforme.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnInforme.Name = "btnInforme";
-            this.btnInforme.Size = new System.Drawing.Size(121, 37);
+            this.btnInforme.Size = new System.Drawing.Size(91, 30);
             this.btnInforme.TabIndex = 2;
             this.btnInforme.Text = "Enviar informe";
             this.btnInforme.UseVisualStyleBackColor = true;
@@ -79,19 +82,21 @@
             // 
             // txtInforme
             // 
-            this.txtInforme.Location = new System.Drawing.Point(3, 27);
+            this.txtInforme.Location = new System.Drawing.Point(2, 22);
+            this.txtInforme.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtInforme.Multiline = true;
             this.txtInforme.Name = "txtInforme";
-            this.txtInforme.Size = new System.Drawing.Size(562, 155);
+            this.txtInforme.Size = new System.Drawing.Size(422, 127);
             this.txtInforme.TabIndex = 1;
             // 
             // lbltitle
             // 
             this.lbltitle.AutoSize = true;
             this.lbltitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbltitle.Location = new System.Drawing.Point(66, 8);
+            this.lbltitle.Location = new System.Drawing.Point(50, 6);
+            this.lbltitle.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbltitle.Name = "lbltitle";
-            this.lbltitle.Size = new System.Drawing.Size(437, 16);
+            this.lbltitle.Size = new System.Drawing.Size(355, 13);
             this.lbltitle.TabIndex = 0;
             this.lbltitle.Text = "Por favor, describa el error de la manera más precisa posible:";
             // 
@@ -101,10 +106,11 @@
             this.tabPage2.Controls.Add(this.btnSolMail);
             this.tabPage2.Controls.Add(this.txtSolMail);
             this.tabPage2.Controls.Add(this.lblrecup);
-            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(568, 231);
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage2.Size = new System.Drawing.Size(424, 185);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Recuperar contraseña";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -113,17 +119,19 @@
             // 
             this.lblMess.AutoSize = true;
             this.lblMess.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblMess.Location = new System.Drawing.Point(45, 199);
+            this.lblMess.Location = new System.Drawing.Point(34, 162);
+            this.lblMess.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblMess.Name = "lblMess";
-            this.lblMess.Size = new System.Drawing.Size(453, 16);
+            this.lblMess.Size = new System.Drawing.Size(379, 13);
             this.lblMess.TabIndex = 3;
             this.lblMess.Text = "Un administrador contactara con usted con su nueva contraseña.";
             // 
             // btnSolMail
             // 
-            this.btnSolMail.Location = new System.Drawing.Point(216, 130);
+            this.btnSolMail.Location = new System.Drawing.Point(162, 106);
+            this.btnSolMail.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnSolMail.Name = "btnSolMail";
-            this.btnSolMail.Size = new System.Drawing.Size(116, 32);
+            this.btnSolMail.Size = new System.Drawing.Size(87, 26);
             this.btnSolMail.TabIndex = 2;
             this.btnSolMail.Text = "Enviar";
             this.btnSolMail.UseVisualStyleBackColor = true;
@@ -131,27 +139,30 @@
             // 
             // txtSolMail
             // 
-            this.txtSolMail.Location = new System.Drawing.Point(102, 102);
+            this.txtSolMail.Location = new System.Drawing.Point(76, 83);
+            this.txtSolMail.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtSolMail.Name = "txtSolMail";
-            this.txtSolMail.Size = new System.Drawing.Size(354, 22);
+            this.txtSolMail.Size = new System.Drawing.Size(266, 20);
             this.txtSolMail.TabIndex = 1;
             // 
             // lblrecup
             // 
             this.lblrecup.AutoSize = true;
             this.lblrecup.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblrecup.Location = new System.Drawing.Point(99, 74);
+            this.lblrecup.Location = new System.Drawing.Point(74, 60);
+            this.lblrecup.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblrecup.Name = "lblrecup";
-            this.lblrecup.Size = new System.Drawing.Size(357, 16);
+            this.lblrecup.Size = new System.Drawing.Size(297, 13);
             this.lblrecup.TabIndex = 0;
             this.lblrecup.Text = "Inserte el correo electrónico asociado a su cuenta:";
             // 
             // Soporte
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(581, 262);
+            this.ClientSize = new System.Drawing.Size(436, 213);
             this.Controls.Add(this.tabSoporte);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Soporte";
             this.Text = "Soporte";
             this.Load += new System.EventHandler(this.Soporte_Load);
