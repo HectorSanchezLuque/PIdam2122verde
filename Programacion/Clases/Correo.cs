97c01741d49bde5d7573b29dc99a16a2f3cbe4b5
Hector.Luque <hesalu@floridauniversitaria.es>
2022-05-20 18:22:33 +0200

    super mega ok

--- Programacion/Clases/Correo.cs
+++ Programacion/Clases/Correo.cs
@@ -54,7 +54,7 @@ namespace ProyectoIntegradoVerde.Clases
         {
             int retorno;
 
-                string consulta = String.Format("INSERT INTO Correos (idCorreo,asunto,cuerpo,recipiente,remitente,fecha,usuarios_id) VALUES " +
+                string consulta = String.Format("INSERT INTO Correos (idCorreo,asunto,cuerpo,recipiente,remitente,fecha) VALUES " +
                     "('{0}','{1}','{2}','{3}','{4}','{5}');", cor.id, cor.asunto, cor.cuerpo, cor.recipiente, cor.remitente, cor.fecha);
 
                 MySqlCommand comando = new MySqlCommand(consulta, conexion.Conexion);
