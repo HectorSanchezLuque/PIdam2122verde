9cf7f4904df12e3b85451af5ccb65fe8d411364c
Alvaro Pérez Alborch <alpeal01@floridauniversitaria.es>
2022-05-22 19:08:42 +0200

    arreglo bd

---
+++ BBDD/Inserts.sql
@@ -0,0 +1,3 @@
+INSERT INTO usuarios (nif, nombre, fecha_nac, cargo, puntos, correo, pswd) VALUES("12345678Z", "persona", "1990-10-14", "Jefe", 0, "personajefe@gmai.com", "jefe123");
+
+INSERT INTO tarea (titulo, descripcion, puntos, asignado, finalizado, fecha_publicacion, fecha_limite) VALUES ("Hacer cositas", "Cositas", 100, 0, 0, '2022-05-11', '2022-05-17');
